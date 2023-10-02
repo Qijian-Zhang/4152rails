51afee3dd59184318536a76bbafee4be9c269944
Qijian Zhang <qz2507@columbia.edu>
2023-10-02 03:46:48 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,7 +21,7 @@ class Movie < ActiveRecord::Base
   end
 
   def self.sort_by_name(ratings_list)
-    if ratings_list.empty?
+    if ratings_list.nil? or ratings_list.empty?
       movies=Movie.order(:title)
     else
       movies = Movie.where(rating: ratings_list).order(:title)
@@ -30,7 +30,7 @@ class Movie < ActiveRecord::Base
   end
 
   def self.sort_by_time(ratings_list)
-    if ratings_list.empty?
+    if ratings_list.nil? or ratings_list.empty?
       movies=Movie.order(:release_date)
     else
       movies = Movie.where(rating: ratings_list).order(:release_date)
